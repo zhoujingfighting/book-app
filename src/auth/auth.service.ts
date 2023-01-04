b31b96e494517de1c72ca31f4eb43b09ec7f6114
zhoujingfighting <254644528@qq.com>
2023-01-04 22:55:48 +0800

    Update signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, Req } from '@nestjs/common';
+import { ForbiddenException, Injectable, Req } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client';
 import { Request } from 'express';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon2 from 'argon2';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
@@ -16,16 +17,29 @@ export class AuthService {
     // First step : create hash password
     const hash = await argon2.hash(body.password);
     // Second step : create user
-    const user = await this.prisma.user.create({
-      data: {
-        hash,
-        email: body.email,
-      },
-      select: {
-        email: true,
-      },
-    });
+    // Need to catch error when signup process comes some errors
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          hash,
+          email: body.email,
+        },
+        select: {
+          // choose what to sed to frontend
+          email: true,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credential only');
+          // Check ForbiddenException meaning
+        }
+      }
+      throw error;
+    }
+
     // Third step : return created user
-    return user;
   }
 }
