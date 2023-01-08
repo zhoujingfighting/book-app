8abc90720a6c66bf4adcae5846f5ed18221df63c
zhoujingfighting <254644528@qq.com>
2023-01-08 20:31:12 +0800

    Update config module use

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest?schema=public',
+          url: configService.get('DATABASE_URL'),
         },
       },
     });
+    console.log(configService, 11);
   }
 }
